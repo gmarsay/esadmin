c02640811a1d8740e36c94d008f5cee3392d2ed2
Guillaume Marsay <gmarsay@gmail.com>
2017-06-04 00:01:16 +0200

    Add composer.json Ignore vendor

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <html>
 <head>
   <title>ES Admin</title>
-  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  <link href="assets/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href="vendor/components/bootstrap/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link href="vendor/components/font-awesome/css/font-awesome.min.css" rel="stylesheet">
   <link href="assets/styles.css" rel="stylesheet">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <script src="vendor/components/jquery/jquery.min.js"></script>
+  <script src="vendor/components/bootstrap/js/bootstrap.min.js"></script>
   <script src="assets/app.js"></script>
 </head>
 <body>
@@ -95,11 +95,5 @@
     </div>
   </div>
 </footer>
-
-<script>
-$(document).ready(function() {
-  $('.dropdown-toggle').dropdown();
-});
-</script>
 </body>
 </html>
